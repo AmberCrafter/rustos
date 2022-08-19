2df6c69f92b4fa0c9c0098e549ac1ad2a8b39497
ambercrafter <flamingm321@gmail.com>
2022-08-19 17:45:02 +0800

    Init

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+#![no_std]
+#![no_main]
+#![feature(lang_items)]
+
+use bootloader::{entry_point, BootInfo};
+use core::panic::PanicInfo;
+
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
+    // turn the screen gray
+    if let Some(framebuffer) = boot_info.framebuffer.as_mut() {
+        for byte in framebuffer.buffer_mut() {
+            *byte = 0x90;
+        }
+    }
+
+    loop {}
+}
+
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[lang = "eh_personality"]
+#[no_mangle]
+pub extern fn eh_personality() {}
