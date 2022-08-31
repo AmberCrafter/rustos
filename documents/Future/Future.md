a60a5b21c4aa2a0b1e54ccaaeea3a712b84d6195
LeisurelyAzure <39610187+AmberCrafter@users.noreply.github.com>
2022-08-31 02:43:06 +0000

    Implement task executor

--- documents/Future/Future.md
+++ documents/Future/Future.md
@@ -1,3 +1,17 @@
 # Future
 
 [](./FutureImplementation%20Memo.png)
+
+[](./FutureImplementationII.png)
+
+
+# Reference
+- blog_os
+    en: https://os.phil-opp.com/async-await/
+
+    zh: https://github.com/rustcc/writing-an-os-in-rust/blob/master/12-async-await.md
+
+- rust async-book
+    en: https://rust-lang.github.io/async-book/02_execution/02_future.html
+
+    zh: https://weihanglo.tw/async-book/02_execution/04_executor.html
--- src/library/task/mod.rs
+++ src/library/task/mod.rs
@@ -1,19 +1,34 @@
-pub mod simple_executor;
+// pub mod simple_executor;
+pub mod executor;
 pub mod keyboard;
 
-use core::{pin::Pin, future::Future, task::{Context, Poll}};
+use core::{pin::Pin, future::Future, task::{Context, Poll}, sync::atomic::{AtomicU64, Ordering::Relaxed}};
 use alloc::boxed::Box;
 
-use self::simple_executor::SimpleExecutor;
+// use self::simple_executor::SimpleExecutor;
+use self::executor::Executor;
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct TaskId(u64);
+
+impl TaskId {
+    fn new() -> Self {
+        static NEXT_ID: AtomicU64 = AtomicU64::new(0);
+        Self(NEXT_ID.fetch_add(1, Relaxed))
+    }
+}
 
 
 pub struct Task {
+    id: TaskId,
     future: Pin<Box<dyn Future<Output = ()>>>
 }
 
 impl Task {
     pub fn new(future: impl Future<Output = ()> + 'static) -> Self {
         Self {
+            id: TaskId::new(),
             future: Box::pin(future)
         }
     }
@@ -25,7 +40,8 @@ impl Task {
 
 
 pub fn init() {
-    let mut executor = SimpleExecutor::new();
+    // let mut executor = SimpleExecutor::new();
+    let mut executor = Executor::new();
     executor.spawn(Task::new(keyboard::execute_keycode()));
     executor.run();
 }
